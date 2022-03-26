cf92d10d0079c6125bb42c2cff4a6ddc89cf5a11
Berestov <berestovvanya@gmail.com>
2022-03-26 17:16:01 +0200

    Add project files.

---
+++ WebForumMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebForumMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
