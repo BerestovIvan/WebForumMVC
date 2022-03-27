46f309b444511b0c717ac901c1ccb9bfa00f9e90
Berestov <berestovvanya@gmail.com>
2022-03-27 13:03:33 +0300

    Added comment service

---
+++ BLL/ServiceInterfaces/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.ServiceInterfaces
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentModel>> Get();
+        Task<CommentModel> Get(Guid id);
+        Task<CommentModel> Create(CommentModel commentModel);
+        Task<CommentModel> Update(CommentModel commentModel);
+        Task Delete(Guid id);
+    }
+}
---
+++ BLL/Services/CommentService.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using BLL.Exceptions;
+using BLL.Models;
+using BLL.ServiceInterfaces;
+using DAL.Entity;
+using DAL.RepositoriesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CommentService : ICommentService
+    {
+        readonly ICommentRepository commentRepository;
+        readonly IMapper mapper;
+        public CommentService(ICommentRepository commentRepository, IMapper mapper)
+        {
+            this.commentRepository = commentRepository;
+            this.mapper = mapper;
+        }
+        public async Task<CommentModel> Create(CommentModel commentModel)
+        {
+            commentModel.Id = Guid.NewGuid();
+            await commentRepository.Create(mapper.Map<Comment>(commentModel));
+            return commentModel;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var model = await commentRepository.Get(id);
+            if (model == null)
+            {
+                throw new EntityNotFoundException("This comment doesn't exists");
+            }
+            else
+            {
+                await commentRepository.Delete(model);
+            }
+        }
+
+        public async Task<IEnumerable<CommentModel>> Get()
+        {
+            var comments = await commentRepository.Get();
+            return mapper.Map<IEnumerable<CommentModel>>(comments);
+        }
+
+        public async Task<CommentModel> Get(Guid id)
+        {
+            var comment = await commentRepository.Get(id);
+            if (comment == null)
+            {
+                throw new EntityNotFoundException("This comment doesn't exists");
+            }
+            else
+            {
+                return mapper.Map<CommentModel>(comment);
+            }
+        }
+
+        public async Task<CommentModel> Update(CommentModel commentModel)
+        {
+            var comment = await commentRepository.Get(commentModel.Id);
+            if (comment == null)
+            {
+                throw new EntityNotFoundException("This comment doesn't exists");
+            }
+            else
+            {
+                comment = await commentRepository.Update(mapper.Map<Comment>(commentModel));
+                return mapper.Map<CommentModel>(comment);
+            }
+        }
+    }
+}
