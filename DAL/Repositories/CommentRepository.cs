df3a3b417fd8a929b8b804a0fb070cefe4363c32
Berestov <berestovvanya@gmail.com>
2022-03-27 13:00:24 +0300

    Added comment repository

---
+++ DAL/Repositories/CommentRepository.cs
@@ -0,0 +1,50 @@
+﻿using DAL.DbContext;
+using DAL.Entity;
+using DAL.RepositoriesInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        readonly ApplicationDbContext context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Comment>> Get()
+        {
+            return await context.Comments.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Comment> Get(Guid id)
+        {
+            return await context.Comments.AsNoTracking().FirstOrDefaultAsync(comment => comment.Id == id);
+        }
+
+        public async Task<Comment> Create(Comment comment)
+        {
+            context.Comments.Add(comment);
+            await context.SaveChangesAsync();
+            return comment;
+        }
+
+        public async Task<Comment> Update(Comment comment)
+        {
+            context.Comments.Update(comment);
+            await context.SaveChangesAsync();
+            return comment;
+        }
+
+        public async Task Delete(Comment comment)
+        {
+            context.Comments.Remove(comment);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DAL/RepositoriesInterfaces/ICommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.RepositoriesInterfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> Get();
+        Task<Comment> Get(Guid id);
+        Task<Comment> Create(Comment comment);
+        Task<Comment> Update(Comment comment);
+        Task Delete(Comment comment);
+    }
+}
