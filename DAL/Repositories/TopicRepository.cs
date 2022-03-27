51a89ecef601e3ad3713b764f5d31866fe417a2b
Berestov <berestovvanya@gmail.com>
2022-03-27 09:38:17 +0300

    Added topic repository

---
+++ DAL/Repositories/TopicRepository.cs
@@ -0,0 +1,55 @@
+﻿using DAL.DbContext;
+using DAL.Entity;
+using DAL.RepositoriesInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class TopicRepository : ITopicRepository
+    {
+        readonly ApplicationDbContext context;
+
+        public TopicRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Topic>> Get()
+        {
+            return await context.Topics.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Topic> Get(Guid id)
+        {
+            return await context.Topics.AsNoTracking().FirstOrDefaultAsync(topic => topic.Id == id);
+        }
+
+        public async Task<Topic> Get(string title)
+        {
+            return await context.Topics.AsNoTracking().FirstOrDefaultAsync(topic => topic.Title == title);
+        }
+
+        public async Task<Topic> Create(Topic topic)
+        {
+            context.Topics.Add(topic);
+            await context.SaveChangesAsync();
+            return topic;
+        }
+
+        public async Task<Topic> Update(Topic topic)
+        {
+            context.Topics.Update(topic);
+            await context.SaveChangesAsync();
+            return topic;
+        }
+
+        public async Task Delete(Topic topic)
+        {
+            context.Topics.Remove(topic);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DAL/RepositoriesInterfaces/ITopicRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.RepositoriesInterfaces
+{
+    public interface ITopicRepository
+    {
+        Task<IEnumerable<Topic>> Get();
+        Task<Topic> Get(Guid id);
+        Task<Topic> Get(string title);
+        Task<Topic> Create(Topic topic);
+        Task<Topic> Update(Topic topic);
+        Task Delete(Topic topic);
+    }
+}
