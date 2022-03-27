8fb72ec4cbc9665ab63fdb6bfc6e93efd2e5b38b
Berestov <berestovvanya@gmail.com>
2022-03-27 11:38:31 +0300

    added article repository

---
+++ DAL/Repositories/ArticleRepository.cs
@@ -0,0 +1,59 @@
+﻿using DAL.DbContext;
+using DAL.Entity;
+using DAL.RepositoriesInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        readonly ApplicationDbContext context;
+
+        public ArticleRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Article>> Get()
+        {
+            return await context.Articles.
+                Include(x => x.Comments).
+                Include(x => x.Creator).
+                Include(x => x.Topic).
+                AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Article> Get(Guid id)
+        {
+            return await context.Articles.
+                Include(x => x.Comments).
+                Include(x => x.Creator).
+                Include(x => x.Topic).
+                AsNoTracking().
+                FirstOrDefaultAsync(article => article.Id == id);
+        }
+
+        public async Task<Article> Create(Article article)
+        {
+            context.Articles.Add(article);
+            await context.SaveChangesAsync();
+            return article;
+        }
+
+        public async Task<Article> Update(Article article)
+        {
+            context.Articles.Update(article);
+            await context.SaveChangesAsync();
+            return article;
+        }
+
+        public async Task Delete(Article article)
+        {
+            context.Articles.Remove(article);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DAL/RepositoriesInterfaces/IArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.RepositoriesInterfaces
+{
+    public interface IArticleRepository
+    {
+        Task<IEnumerable<Article>> Get();
+        Task<Article> Get(Guid id);
+        Task<Article> Create(Article article);
+        Task<Article> Update(Article article);
+        Task Delete(Article article);
+    }
+}
